256a204bc5ddbb8b5fccb7efe232f9aa020c132a
Yosselinne Navarro <yosselinne.navarro@gmail.com>
2026-03-10 01:24:22 -0300

    Main v1

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const formulario = document.getElementById("formulario");
+
+        formulario.addEventListener("submit", function(event) {
+            event.preventDefault(); // Evita que se recargue la página
+
+            alert("✅ Registro exitoso");
+
+            formulario.reset(); // Limpia todos los campos
+        });
+        
